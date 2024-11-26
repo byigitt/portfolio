650a3be3e121f137eaf4c2a86f4ffb564e50646f
byigitt <c2228009@student.cankaya.edu.tr>
2024-11-26 14:28:50 +0300

    feat: Update Next.js configuration and add blog generation script

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
+import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm]
   },
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
+  output: 'export' as const,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  }
 }
 
 export default withMDX(nextConfig)
